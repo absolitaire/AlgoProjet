6e1a116e0fdb274a025bc7f9f540c256ccf5014b
Thiebaud Marbach <thiebaud.marbach2@etu.univ-lorraine.fr>
2018-12-18 14:54:32 +0100

    fichiers fournis

---
+++ src/modelisation/Edge.java
@@ -0,0 +1,18 @@
+package modelisation;
+class Edge
+{
+   int from;
+   int to;
+   boolean used;
+    Edge(int x, int y)
+    {
+	this.from = x;
+	this.to = y;
+	this.used = false;
+    }
+    
+    final int other(int v)
+    {
+	if (this.from == v) return this.to; else return this.from;
+    }    
+}
